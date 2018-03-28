be6b31010aef8cd9da7a44162a01e58f21f6aef6
Edrenn <edern.rodriguez@orange.fr>
2018-03-28 14:34:53 +0200

    First commit

---
+++ dafront/src/components/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1>{{ msg }} {{title}}</h1>
+    <h4>Serveurs : </h4>
+    <ul>
+      <li><a href="">Serveur 1</a></li>
+      <li><a href="">Serveur 2</a></li>
+    </ul>
+    <div id="app">
+      <line-chart :data="data" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Graph from server : ',
+      title: 'srv-DC-london_global',
+      data: [
+        {
+          name: 'cpu0',
+          data: {
+          '1515020486': 49.620000,
+          '1515020786':62.420000,
+          '1515021086': 47.970000
+          }
+        },
+        {
+          name: 'cpu1',
+          data: {
+          '1515020486': 60.310000,
+          '1515020786':66.060000,
+          '1515021086': 48.030000
+          }
+        }
+      ]
+    }
+  }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ dafront/src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import Chartkick from 'chartkick'
+import VueChartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+
+Vue.config.productionTip = false
+Vue.use(VueChartkick, { Chartkick })
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
